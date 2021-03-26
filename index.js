73284c7437b7851e2710973ca2125557a260012a
Zanzong43 <alexkoszuta43@gmail.com>
2021-03-26 14:23:49 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Odds 1-20
+for (var i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+
+//Sequence
+for (var i = 4; i > -3.5; i -= 1.5) {
+    console.log(i);
+}
+
+//Sigma
+var sum = 0;
+for (var i = 0; i <= 100; i++) {
+    console.log(i);
+    sum = sum + i;
+}
+console.log(sum);
+
+//Factorial
+var product = 1;
+for (var n = 1; product < 1e9; n++) {
+    product = product * n;
+}
+console.log(n);
